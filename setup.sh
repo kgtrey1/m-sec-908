12507f002956bf7d29937e4f6fec2574aa65fe22
Kevin <kevin.gouyet@protonmail.com>
2024-10-30 17:14:13 +0800

    fix: ssh setup

--- setup.sh
+++ setup.sh
@@ -10,6 +10,18 @@ sudo useradd -m -s /bin/bash powerzio
 echo "powerzio:123456" | sudo chpasswd
 sudo usermod -aG sudo powerzio
 
+
+
+# Setup SSH
+sudo mkdir -p /home/powerzio/.ssh
+sudo cp config/local/authorized_keys /home/powerzio/.ssh/authorized_keys
+sudo chmod 700 /home/powerzio/.ssh
+sudo chmod 600 /home/powerzio/.ssh/authorized_keys
+sudo chown -R powerzio:powerzio /home/powerzio/.ssh
+sudo cp config/local/sshd_config /etc/ssh/sshd_config
+sudo systemctl restart ssh
+
+
 # Docker install
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -22,16 +34,6 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 sudo usermod -aG docker powerzio
 sudo newgrp docker
-
-# Setup SSH
-sudo mkdir -p /home/powerzio/.ssh
-sudo cp config/local/authorized_keys /home/powerzio/.ssh/authorized_keys
-sudo chmod 700 /home/powerzio/.ssh
-sudo chmod 600 /home/powerzio/.ssh/authorized_keys
-sudo chown -R powerzio:powerzio /home/powerzio/.ssh
-sudo cp config/local/sshd_config /etc/ssh/sshd_config
-sudo systemctl restart ssh
-
 
 # Free port 53
 # sudo systemctl stop systemd-resolved
